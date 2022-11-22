31361f9b3dd4cabb26d568d91d4b8179e3a56cb6
Vyacheslav Lyubchich <vlyubchich@users.noreply.github.com>
2022-11-22 16:12:12 -0500

    update ex1

--- code/hw1_ex1.R
+++ code/hw1_ex1.R
@@ -1,26 +1,31 @@
-myplot <- function(df, main = my_title, xlab = xlabel, ylab = ylabel,
-                   col = "black", plotmean = TRUE, ...){
-    df <- as.matrix(df, na.omit(df))
-    if (is.data.frame(df)) { all(dim(df) == c(1,1))}
-    if (is.matrix(df) == TRUE) {
+myplot <- function(df, colmed = "black", plotmean = TRUE, ...){
+    if (is.matrix(df)) {
         print("successful check")
     } else {
-        stop("Not correct data format")}
-    plotmean <- apply(df, 1, mean, na.rm = TRUE)
+        df <- as.matrix(df)
+    }
+    if (dim(df)[2] == 1) {
+        stop("Not correct data format. The input data must have >1 column.")
+    }
+    m <- apply(df, 1, mean, na.rm = TRUE)
+    med <- apply(df, 1, median, na.rm = TRUE)
     q1 <- apply(df, 1, quantile, na.rm = TRUE, probs = 0.25)
     q3 <- apply(df, 1, quantile, na.rm = TRUE, probs = 0.75)
-    df2 <- return(data.frame(Mean = plotmean, Q1 = q1, Q3 = q3))
-    df3 <- as.numeric(df2)
+    # df2 <- data.frame(Mean = m, med = med, Q1 = q1, Q3 = q3)
+    # df3 <- as.numeric(df2)
     tmp = as.numeric(time(df))
-    plot(df3, main = "my_title", xlab = "xlabel", ylab = "ylabel",
-         las = 1,
+    plot(x = tmp, y = med, las = 1,
          pch = 18,
          col = "black",
+         ylim = range(c(q1, q3)),
          cex = 2,
          type = "b",
-         lines(x = tmp, y = plotmean, col = "blue", lty = 2),
-         lines(x = tmp, y = q1, col = "green"),
-         lines(x = tmp, y = q3, col = "red"),
-         legend("topleft", legend = c("Mean", "Average", "Q1 & Q3"),
-                col = c("blue", "green", "red")))
+         ...)
+    polygon(x = c(tmp, rev(tmp)), y = c(q1, rev(q3)), col = "green")
+    if (plotmean) {
+        lines(x = tmp, y = m, col = "blue", lty = 2)
+    }
+    lines(x = tmp, y = med, col = colmed, lwd = 2)
+    legend("topleft", legend = c("Mean", "Average", "Q1 & Q3"),
+                col = c("blue", "green", "red"))
 }
