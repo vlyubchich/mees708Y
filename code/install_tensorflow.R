d7b2153a2e53668266de52b4f91fa9afb0571717
Vyacheslav Lyubchich <vlyubchich@users.noreply.github.com>
2022-12-06 14:48:18 -0500

    tf install

---
+++ code/install_tensorflow.R
@@ -0,0 +1,6 @@
+install.packages("keras")
+install.packages("tensorflow")
+library(keras)
+library(tensorflow)
+tensorflow::install_tensorflow(version = "2.7")
+# When asked to install Miniconda, type Y in the Console and press Enter.
